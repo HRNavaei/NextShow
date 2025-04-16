536dbdc7c744e4f279020335235c041729dc3ead
Hamidreza Navaei <HRNavaei@yahoo.com>
2025-04-16 21:43:07 +0330

    fix: add default port number in config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ dotenv.config({ path: './.env' });
 
 export default {
   NODE_ENV: config.NODE_ENV,
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 3000,
   DB_URI: process.env.DB_URI,
   JWT_SECRET: process.env.JWT_SECRET,
 };
